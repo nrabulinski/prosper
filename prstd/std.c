102aba6f3a9cd6211231536bbc1f78b1781fb647
Nikodem Rabuliński <nikodemrabulinski@gmail.com>
2020-08-31 23:17:02 +0200

    std fix

--- prstd/std.c
+++ prstd/std.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
 #include<stdbool.h>
 #include<stdint.h>
+#include<inttypes.h>
 
 void print_bool(bool b) {
     printf(b ? "true\n" : "false\n");
 }
 
 void print_i32(int32_t i) {
-    printf("%d\n", i);
+    printf("%" PRIi32 "\n", i);
+}
+
+void print_u64(uint64_t i) {
+    printf("%" PRIu64 "\n", i);
 }
 
 void print_f64(double f) {
